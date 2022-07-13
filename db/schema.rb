87ff7e0b7a2eade7c179da378c255027a96e377f
FtobarV64 <ftobarv@gmail.com>
2022-07-12 21:58:04 -0400

    Creating MVC to Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_014822) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "reference"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end

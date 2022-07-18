437ee775ee74aeca96c0fb047fa9fa3dc270903c
FtobarV64 <ftobarv@gmail.com>
2022-07-18 19:47:56 -0400

    Adding export to Excel

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,10 +4,19 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html
+      format.xlsx{response.headers['Content-Disposition']='attachment; filename="ListadoProd.xlsx"'}
+    end
   end
 
   # GET /products/1 or /products/1.json
   def show
+    respond_to do |format|
+      format.html
+      format.xlsx{response.headers['Content-Disposition']='attachment; filename="DetalleProd.xlsx"'}
+    end
   end
 
   # GET /products/new
